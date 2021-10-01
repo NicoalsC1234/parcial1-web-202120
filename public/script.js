5fd09ec4751fa0327caf2f040920739c87039df3
NicoalsC1234 <nc.guerrero@uniandes.edu.co>
2021-10-01 14:27:54 -0500

    parcial 1

--- public/script.js
+++ public/script.js
@@ -7,9 +7,15 @@ const message = document.getElementById('message');
 
 enterButton.addEventListener('click', (event) => {
   //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+  fetch('https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players')
+  .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data)
+      //getresults(input.value);
+    });
   event.preventDefault();
+  
+  
 });
 
 /**
@@ -19,7 +25,28 @@ enterButton.addEventListener('click', (event) => {
 async function getresults(heightRef) {
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
-  console.log('data from back', data);
+
+  let i = 1
+  data.forEach(element => {
+    let tr = document.createElement("tr")
+    let td = document.createElement("td")
+    let td2 = document.createElement("td")
+    let th = document.createElement("th")
+
+    th.appendChild(i)
+    td.appendChild(element[0].first_name + " " + element[0].last_name)
+    td2.appendChild(element[1].first_name + " " + element[1].last_name)
+
+    tr.appendChild(th)
+    tr.appendChild(td)
+    tr.appendChild(td2)
+    tbody.appendChild(tr)
+
+    i++
+  });
+
+
+  console.log(data)
   //printValues(data);
 }
 
@@ -28,3 +55,5 @@ function removeAllChildNodes(parent) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+
--- src/controller.js
+++ src/controller.js
@@ -2,7 +2,24 @@ const { response, request } = require('express');
 
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
   //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  //Asumiendo que data es el arreglo que me llega de ljson :)
+  let ar = []
+  let l = []
+  for (let index = 0; index < data.length; index++) {
+    const suma1 = data[index].h_in;
+    for (let index2 = index+1; index2 < data.length; index2++) {
+      const suma2 = data[index2].h_in;
+      if(suma1+suma1 === resp){
+        l.push(data[index])
+        l.push(data[index2])
+        ar.push(l)
+      }
+
+    }
+  }
+
+  
+  return resp.json(ar);
 };
 
 module.exports = { getPairsOfPlayers };
